c2a18f3d57e58dabc360f8600b6a780b2a793c9e
David Szczepanik <106731343+David-Szczepanik@users.noreply.github.com>
2024-02-27 12:18:14 +0100

    fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,34 +7,38 @@ public class Main {
   public static void main(String[] args) {
 
     System.out.println("---CD Video Pole---");
-    Media[] mediaPole = new Media[] {
-    new CD("CD1", "Interpret1", 60, 300),
-    new CD("CD2", "Interpret2", 70, 350),
-    new Video("Video1", "Reziser1", 120, 200),
-    new Video("Video2", "Reziser2", 150, 250)
-};
+
+    CD cd1 = new CD("CD1", "Interpret1", 60, 300);
+    CD cd2 = new CD("CD2", "Interpret2", 70, 350);
+    Video video1 = new Video("Video1", "Reziser1", 120, 200);
+    Video video2 = new Video("Video2", "Reziser2", 150, 250);
+
+    Media[] mediaPole = new Media[]{
+        cd1, cd2, video1, video2
+    };
+
     for (Media media : mediaPole) {
-    System.out.println("Titul: " + media.getTitle());
-    System.out.println("Cena: " + media.getPrice());
-}
+      System.out.println("Titul: " + media.getTitle());
+      System.out.println("Cena: " + media.getPrice());
+    }
+
 
     System.out.println("\n---Knihy Pole---");
-    Kniha[] knihaPole = new Kniha[] {
-    new Kniha("Kniha1", "Autor1", 250),
-    new Kniha("Kniha2", "Autor2", 300),
-    new Kniha("Kniha3", "Autor3", 350)
-};
+    Kniha[] knihaPole = new Kniha[]{
+        new Kniha("Kniha1", "Autor1", 250),
+        new Kniha("Kniha2", "Autor2", 300),
+        new Kniha("Kniha3", "Autor3", 350)
+    };
     for (Kniha kniha : knihaPole) {
-    System.out.println("Titul: " + kniha.getTitle());
-    System.out.println("Cena: " + kniha.getPrice());
-}
+      System.out.println("Titul: " + kniha.getTitle());
+      System.out.println("Cena: " + kniha.getPrice());
+    }
     System.out.println("\n---Kolekce---");
 
     List<Media> mediaList = new ArrayList<>();
 
-    CD cd1 = new CD("titulCD", "interpret", 20, 300);
-    Video v1 = new Video("video","reziser",60,200);
-    Kniha k1 = new Kniha("Kniha1","autor", 250);
+    Video v1 = new Video("video", "reziser", 60, 200);
+    Kniha k1 = new Kniha("Kniha1", "autor", 250);
 
 
     System.out.println(cd1.getDelka());
@@ -43,11 +47,11 @@ public class Main {
     mediaList.add(v1);
     mediaList.add(k1);
 
-    double celkovaCena= 0;
+    double celkovaCena = 0;
     for (Media media : mediaList) {
       System.out.println("Titul: " + media.getTitle());
       System.out.println("Cena: " + media.getPrice());
-      celkovaCena+= media.getPrice();
+      celkovaCena += media.getPrice();
     }
     System.out.println("Celková cena: " + celkovaCena);
   }
